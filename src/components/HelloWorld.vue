8c267c5b0fe2e883d21b7ff95c4e3d883e172c87
todoi <steelangeltodoi@gmail.com>
2018-03-02 18:54:49 +0800

    What: modified:[build/utils.js, src/App.vue, src/components/HelloWorld.vue]
    Why:
    
    * 按照vue.js 初级教程，熟悉vue 指令
    
    How:
    
    * 参考 vue.js入门基础(http://www.imooc.com/learn/694)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,54 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>{{ title }}</h1>
+    <input type="text" v-model.trim="userInput" placeholder="hello world" v-on:keyup.enter="submit($event)">
     <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
+      <li v-for="item in items"><span v-bind:class="{isFinished: item.isFinished}" v-on:click="toggleClass($event, item)">{{ item.description }}</span></li>
     </ul>
   </div>
 </template>
-
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title: 'TODOLIST',
+      items: [
+        {
+          description: 'coding',
+          isFinished: true
+        },
+        {
+          description: 'call back girlfriend',
+          isFinished: false
+        }
+      ],
+      userInput: ''
+    }
+  },
+  methods: {
+    toggleClass: function($event, item){
+      item.isFinished = !item.isFinished
+      console.log(this)
+    },
+    submit: function($event){
+      console.log($event)
+      this.userInput && this.items.push({
+        description: this.userInput,
+        isFinished: false
+      })
+      this.userInput = ''
+      console.log('input:' + this.userInput)
     }
   }
 }
-</script>
 
+</script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+h1, h2 {font-weight: normal; }
+ul {list-style-type: circle; padding: 0; text-align: left;}
+a {color: #42b983; }
+.hello{max-width: 600px; margin: 0 auto; font-size: 20px;}
+.isFinished{color: red;}
 </style>
