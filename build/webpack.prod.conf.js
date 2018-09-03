4c8c9b740bde70d069cc5ddac0a58c0d1dd7e7c0
todoi <steelangeltodoi@gmail.com>
2018-09-03 21:35:05 +0800

    build

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -115,6 +115,14 @@ const webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
+    ]),
+
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../redirect'),
+        to: config.build.assetsPublicPath,
+        ignore: ['.*']
+      }
     ])
   ]
 })
